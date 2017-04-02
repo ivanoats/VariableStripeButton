e37845dcbd8e1441a8a66f5279252b3d957032d5
Ivan Storck <ivan@ivanstorck.com>
2017-04-02 10:03:02 -0700

    pay button

---
+++ variable-pay-button.js
@@ -0,0 +1,16 @@
+var stripe = require('stripe');
+
+module.exports = function (ctx, req, res) {
+    var companyName = 'Acme Widgets'
+    stripe(ctx.secrets.stripeSecretKey).charges.create({
+        amount: req.amount,
+        currency: 'usd',
+        source: ctx.body.stripeToken,
+        description: `${companyName} purchase` 
+    }, function (error, charge) {
+        var status = error ? 400 : 200;
+        var message = error ? error.message : `Thanks for purchasing from ${companyName}!`; 
+        res.writeHead(status, { 'Content-Type': 'text/html' });
+        return res.end('<h1>' + message + '</h1>');
+    });
+};
