64530077e30a438eeb229886215cb82ae5797fd8
Ivan Storck <ivan@ivanstorck.com>
2017-04-03 18:02:28 -0700

    try redirect with header

--- form-logger.js
+++ form-logger.js
@@ -1,7 +1,13 @@
 module.exports = function (ctx, req, res) {
-  console.log('----------------------CONTEXT---------')
-  console.log(ctx)
-  console.log('----------------------REQUEST---------')
-  console.log(req)
-  res.redirect(301, 'https://variable-stripe-pay.aerobatic.io/thankyou.html')
+  console.log('-----------------------DATA--------')
+  console.log(ctx.data.amount)
+  const amount = ctx.data.amount
+  res.writeHead(
+    301,
+    {
+      Location:
+      `https://variable-stripe-pay.aerobatic.io/thankyou.html?amount=${amount}`
+    }
+  )
+  res.end('redirecting')
 }
