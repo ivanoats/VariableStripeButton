42f15e28af1db2e0b62de6d4913a244be21d4d0d
Ivan Storck <ivan@ivanstorck.com>
2017-04-03 17:14:16 -0700

    eslint config and redirect to thank you page

--- form-logger.js
+++ form-logger.js
@@ -1,9 +1,7 @@
 module.exports = function (ctx, req, res) {
-  console.log("----------------------CONTEXT---------")
+  console.log('----------------------CONTEXT---------')
   console.log(ctx)
-  console.log("----------------------REQUEST---------")
+  console.log('----------------------REQUEST---------')
   console.log(req)
-  var message = `Thanks for submitting a form`;
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  return res.end('<h1>' + message + '</h1>');
-};
+  res.redirect(301, 'https://variable-stripe-pay.aerobatic.io/thankyou.html')
+}
