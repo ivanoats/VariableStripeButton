6249b414ed2ada7cbf4e69e5363669e66f295b43
Ivan Storck <ivan@ivanstorck.com>
2017-04-04 20:39:00 -0700

    remove extra logging

--- variable-pay-button.js
+++ variable-pay-button.js
@@ -1,20 +1,16 @@
-var stripe = require('stripe');
+var stripe = require('stripe')
 
 module.exports = function (ctx, req, res) {
-    var companyName = 'Acme Widgets'
-    console.log("----------------------CONTEXT---------")
-    console.log(ctx)
-    console.log("----------------------REQUEST---------")
-    console.log(req)
-    stripe(ctx.secrets.stripeSecretKey).charges.create({
-        amount: req.amount,
-        currency: 'usd',
-        source: ctx.body.stripeToken,
-        description: `${companyName} purchase` 
-    }, function (error, charge) {
-        var status = error ? 400 : 200;
-        var message = error ? error.message : `Thanks for purchasing from ${companyName}!`; 
-        res.writeHead(status, { 'Content-Type': 'text/html' });
-        return res.end('<h1>' + message + '</h1>');
-    });
-};
+  var companyName = 'Acme Widgets'
+  stripe(ctx.secrets.stripeSecretKey).charges.create({
+    amount: req.amount,
+    currency: 'usd',
+    source: ctx.body.stripeToken,
+    description: `${companyName} purchase`
+  }, function (error, charge) {
+    var status = error ? 400 : 200
+    var message = error ? error.message : `Thanks for purchasing from ${companyName}!`
+    res.writeHead(status, { 'Content-Type': 'text/html' })
+    return res.end('<h1>' + message + '</h1>')
+  })
+}
